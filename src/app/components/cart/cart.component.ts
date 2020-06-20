4731d75beff7b918b62cca3b0952a4560c998f1e
Rageshwari Bodke <ragu.bodke@gmail.com>
2020-06-20 19:53:42 +0530

    ADDED: Code to sign up and sign in API

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit, Input, Output, SimpleChanges, AfterViewInit, ViewChild } from '@angular/core';
 import { Cart } from 'src/app/model/cart';
 import { HttpService } from 'src/app/service/http.service';
-import { EventEmitter } from 'events';
-import { GetBookDetailsComponent } from '../get-book-details/get-book-details.component';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit, AfterViewInit {
+export class CartComponent implements OnInit{
 // @ViewChild(GetBookDetailsComponent) Books;
 // @Input() book: any;
 //book = []
@@ -18,18 +17,19 @@ export class CartComponent implements OnInit, AfterViewInit {
  public amount: number;
  books : any;
  userId = 1;
- // public cartObj = new Cart(1, 101, 1);
- constructor( public httpService: HttpService ) { }
+ imageUrl: string;
+ constructor( public httpService: HttpService, public sanitizer: DomSanitizer) { }
 
  ngOnInit(): void {
    this.isClicked = false;
    this.getBooksFromCart(this.userId);
+  // this.getImageUrl(book);
  }
- ngAfterViewInit() {
-  // this.book = this.Books.bookArray;
-  // this.message = this.child.message
+//  ngAfterViewInit() {
+//   // this.book = this.Books.bookArray;
+//   // this.message = this.child.message
 
-}
+// }
  add() {
    this.amount = 1;
  }
@@ -52,19 +52,24 @@ export class CartComponent implements OnInit, AfterViewInit {
      this.amount = this.amount;
    }
  }
+ getImageUrl(book) {
+  this.imageUrl = book.image;
+  if (this.imageUrl != null) {
+    var firstReplacement = this.imageUrl.replace("'", '');
+    return this.sanitizer.bypassSecurityTrustUrl(firstReplacement.replace("'", ''));
+  }
+}
  getBooksFromCart(userId){
    this.httpService.getBooksFromCart(userId).subscribe(data => {
      this.books = data;
      this.userId = userId;
      console.log('Data in get card', data);
-    // console.log('>>>',this.books.id);
    });
    console.log(userId);
  }
   removeFromCart(book){
     var cartObj = new Cart(this.userId, book.id, 1);
     this.httpService.removeFromcart(cartObj).subscribe(data => {
-     // this.userId = userId
      this.getBooksFromCart(this.userId);
     });
     console.log('Book removed from cart');
