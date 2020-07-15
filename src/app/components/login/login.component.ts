16804eeb077a95affb51643c29374c9094420ea4
roshan1718 <roshanshinde1717@gmail.com>
2020-07-15 18:12:41 +0530

    ADDED: code to login page validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
 import { SignIn } from 'src/app/model/sign-in';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormControl, FormGroupDirective, NgForm, Validators, MinLengthValidator } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,14 +20,24 @@ export class LoginComponent implements OnInit {
   form: any = {};
   isLoginFailed = false;
   roles: string[] = [];
+  NAME_PATTERN = /^[a-zA-Z]{2,}$/;
+
   constructor(private authService: AuthService, private snackBar: MatSnackBar) { }
   public signInObj = new SignIn();
-  ngOnInit() {}
 
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.NAME_PATTERN),
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8)
+  ]);
+  matcher = new MyErrorStateMatcher();
+  ngOnInit() { }
   loginUser() {
     this.authService.loginUser(this.signInObj).subscribe(data => {
       localStorage.setItem('token', data.accessToken);
-      console.log("login data is", data);
     });
   }
 
